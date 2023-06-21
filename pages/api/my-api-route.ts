8b0a1bbe144dd343df73b15f097e80c57dddbf06
Santy1422 <102694038+Santy1422@users.noreply.github.com>
2023-06-21 19:29:21 +0200

    t

--- pages/api/my-api-route.ts
+++ pages/api/my-api-route.ts
@@ -2,12 +2,14 @@ import { run } from '../../scripts/ingest-data';
 import { NextApiRequest, NextApiResponse } from 'next';
 import multer, { MulterError } from 'multer';
 import path from 'path';
-import { Request, ParamsDictionary, ParsedQs } from 'express';
+import { Request, Response } from 'express';
 
-interface CustomNextApiRequest extends NextApiRequest, Request<ParamsDictionary, any, any, ParsedQs> {}
+interface CustomNextApiRequest extends NextApiRequest {
+  file: Express.Multer.File;
+}
 
 const storage = multer.diskStorage({
-  destination: './docs', // Ruta de destino para guardar los archivos PDF
+  destination: './docs',
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
     cb(null, uniqueSuffix + '-' + file.originalname);
@@ -24,7 +26,7 @@ export const config = {
 
 export default async function handler(req: CustomNextApiRequest, res: NextApiResponse) {
   try {
-    await upload.single('pdf')(req, res, async (err: MulterError) => {
+    await upload.single('pdf')(req as Request, res as Response, async (err: MulterError) => {
       if (err) {
         if (err instanceof multer.MulterError) {
           res.status(500).json({ error: 'Failed to upload file' });
